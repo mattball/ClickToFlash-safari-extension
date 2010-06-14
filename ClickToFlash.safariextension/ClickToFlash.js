20eda5f9f1f76ddd28db7ab5938f2b63bae1af5e
Matt Ball <ball.matt@gmail.com>
2010-06-14 00:41:41 -0700

    Break YouTube stuff into its own function (paving the way for asynchronous calls to global HTML)

--- ClickToFlash.safariextension/ClickToFlash.js
+++ ClickToFlash.safariextension/ClickToFlash.js
@@ -51,6 +51,86 @@ ClickToFlash.prototype.getFlashVariable = function(flashVars, key) {
 	return null;
 }
 
+ClickToFlash.prototype.processYouTubeElement = function(element) {
+	var flashvars = element.getAttribute("flashvars");
+	var videoID = this.getFlashVariable(flashvars, "video_id");
+	var videoHash = this.getFlashVariable(flashvars, "t");
+
+	// Check if there's an h264 version available
+	var h264URL = "http://www.youtube.com/get_video?fmt=18&video_id=" + videoID + "&t=" + videoHash;
+	var h264exists = true;
+
+	if (h264exists) {
+		var videoURL = h264URL;
+
+		// Check if there's a high-res h264 version available
+		var youTubeShouldUseHighResH264 = true;
+		if (youTubeShouldUseHighResH264) {
+			var highResH264URL = "http://www.youtube.com/get_video?fmt=22&video_id=" + videoID + "&t=" + videoHash;
+			var highResH264exists = true;
+			if (highResH264exists) {
+				videoURL = highResH264URL;
+			}
+		}
+
+		var videoElement = document.createElement("video");
+		videoElement.src = videoURL;
+		videoElement.setAttribute("controls", "controls");
+		if (this.getFlashVariable(flashvars, "autoplay") == "1") {
+			videoElement.setAttribute("autoplay", "autoplay");
+		}
+		videoElement.style = element.style;
+		videoElement.style.width = element.offsetWidth + "px";
+		videoElement.style.height = element.offsetHeight + "px";
+
+		element.parentNode.replaceChild(videoElement, element);
+	} else {
+		// No h264 version available, so treat it like a normal element
+		this.processFlashElement(element); 
+	}
+}
+
+ClickToFlash.prototype.processFlashElement = function(element) {
+	// Check if it's already in the mapping dictionary
+	// If so, the user must have clicked it already
+	for (j = 0; j < this.elementMapping.length; j++) {
+		if (this.elementMapping[j] == element) {
+			continue;
+		}
+	}
+
+	var placeholderElement = document.createElement("div");
+	placeholderElement.style = element.style;
+	placeholderElement.style.width = element.offsetWidth + "px";
+	placeholderElement.style.height = element.offsetHeight + "px";
+	placeholderElement.className = "clickToFlashPlaceholder";
+
+	var id = this.elementMapping.length;
+	this.elementMapping[id] = element;
+	placeholderElement.id = "ClickToFlashPlaceholder" + id;
+
+	var clickHandler = this;
+	placeholderElement.onclick = function(event){clickHandler.clickPlaceholder(event)};
+
+	element.parentNode.replaceChild(placeholderElement, element);
+	
+	// Don't display the logo if the box is too small
+	if (placeholderElement.offsetWidth > 100 && placeholderElement.offsetHeight > 50) {
+		var verticalPositionElement = document.createElement("div");
+		verticalPositionElement.className = "logoVerticalPosition";
+		placeholderElement.appendChild(verticalPositionElement);
+
+		var horizontalPositionElement = document.createElement("div");
+		horizontalPositionElement.className = "logoHorizontalPosition";
+		verticalPositionElement.appendChild(horizontalPositionElement);
+
+		var logoElement = document.createElement("div");
+		logoElement.innerHTML = "Flash";
+		logoElement.className = "logo";
+		horizontalPositionElement.appendChild(logoElement);
+	}
+}
+
 ClickToFlash.prototype.removeFlash = function() {
 	this.stopListening();
 	
@@ -78,81 +158,14 @@ ClickToFlash.prototype.removeFlash = function() {
 			                  (flashvars && flashvars.indexOf("youtube.com") != -1) ||
 			                  (flashvars && flashvars.indexOf("youtube-nocookie.com") != -1) ||
 			                  (flashvars && flashvars.indexOf("ytimg.com") != -1);
-			
+
 			if (fromYouTube) {
-				var videoID = this.getFlashVariable(flashvars, "video_id");
-				var videoHash = this.getFlashVariable(flashvars, "t");
-				
-				// Check if there's an h264 version available
-				var h264URL = "http://www.youtube.com/get_video?fmt=18&video_id=" + videoID + "&t=" + videoHash;
-				var h264exists = true;
-				if (h264exists) {
-					var videoURL = h264URL;
-					
-					// Check if there's a high-res h264 version available
-					var youTubeShouldUseHighResH264 = true;
-					if (youTubeShouldUseHighResH264) {
-						var highResH264URL = "http://www.youtube.com/get_video?fmt=22&video_id=" + videoID + "&t=" + videoHash;
-						var highResH264exists = true;
-						if (highResH264exists) {
-							videoURL = highResH264URL;
-						}
-					}
-					
-					var videoElement = document.createElement("video");
-					videoElement.src = videoURL;
-					videoElement.setAttribute("controls", "controls");
-					if (this.getFlashVariable(flashvars, "autoplay") == "1") {
-						videoElement.setAttribute("autoplay", "autoplay");
-					}
-					videoElement.style = element.style;
-					videoElement.style.width = element.offsetWidth + "px";
-					videoElement.style.height = element.offsetHeight + "px";
-					
-					element.parentNode.replaceChild(videoElement, element);
-					continue;
-				}
+				this.processYouTubeElement(element);
+				return;
 			}
 		}
 
-		// Check if it's already in the mapping dictionary
-		// If so, the user must have clicked it already
-		for (j = 0; j < this.elementMapping.length; j++) {
-			if (this.elementMapping[j] == element) {
-				continue;
-			}
-		}
-
-		var placeholderElement = document.createElement("div");
-		placeholderElement.style = element.style;
-		placeholderElement.style.width = element.offsetWidth + "px";
-		placeholderElement.style.height = element.offsetHeight + "px";
-		placeholderElement.className = "clickToFlashPlaceholder";
-
-		var id = this.elementMapping.length;
-		this.elementMapping[id] = element;
-		placeholderElement.id = "ClickToFlashPlaceholder" + id;
-
-		var clickHandler = this;
-		placeholderElement.onclick = function(event){clickHandler.clickPlaceholder(event)};
-
-		element.parentNode.replaceChild(placeholderElement, element);
-		
-		// Don't display the logo if the box is too small
-		if (placeholderElement.offsetWidth > 100 && placeholderElement.offsetHeight > 50) {
-			var verticalPositionElement = document.createElement("div");
-			verticalPositionElement.className = "logoVerticalPosition";
-			placeholderElement.appendChild(verticalPositionElement);
-
-			var horizontalPositionElement = document.createElement("div");
-			horizontalPositionElement.className = "logoHorizontalPosition";
-			verticalPositionElement.appendChild(horizontalPositionElement);
-
-			var logoElement = document.createElement("div");
-			logoElement.innerHTML = "Flash";
-			logoElement.className = "logo";
-			horizontalPositionElement.appendChild(logoElement);
-		}
+		this.processFlashElement(element);
 	}
 
 	this.startListening();
